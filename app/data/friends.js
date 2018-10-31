8814ce0424fbcf2d02638e14457f2cd3f38bd954
William B. Swift <willbswift@hotmail.com>
2018-10-31 18:24:39 -0500

    Some progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,28 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the waitlist tables.
+// Below data will hold all of the info on each user.
 // Initially we just set it equal to a "dummy" customer.
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var waitingArray = [
+let friendArray = [
   {
-    customerName: "Saima",
-    customerEmail: "saima@example.com",
-    phoneNumber: "000-000-0000",
-    customerID: "saimaCool"
+    name:"Ahmed",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
   }
 ];
 
-
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = waitingArray;
+module.exports = friendArray;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
-var bodyParser = require("body-parser");
+let express = require("express");
+let bodyParser = require("body-parser");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -12,10 +12,10 @@ var bodyParser = require("body-parser");
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
-var app = express();
+let app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,8 +27,8 @@ app.use(bodyParser.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
